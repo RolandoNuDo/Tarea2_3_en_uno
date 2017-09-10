60ce3efe1d2657cb1d95beea9ce4c15a96915274
RolandoNuDo <rolo_xo@outlook.com>
2017-09-10 15:24:57 -0600

    Initial commit

---
+++ app/src/main/java/com/rolando/tarea2_3_en_uno/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rolando.tarea2_3_en_uno;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
