a27c4cc1cfd0d10adcf058978a8b1fa3e44a98f2
RolandoNuDo <rolo_xo@outlook.com>
2017-09-11 17:14:06 -0600

    holi

--- app/src/main/java/com/rolando/tarea2_3_en_uno/BusquedaOrdenar.java
+++ app/src/main/java/com/rolando/tarea2_3_en_uno/BusquedaOrdenar.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BusquedaOrdenar extends AppCompatActivity {
-    Button siguiente, anterior;
+    Button siguiente, anterior, odernar;
+    EditText datos;
+    TextView mostrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,24 @@ public class BusquedaOrdenar extends AppCompatActivity {
 
         siguiente = (Button) findViewById(R.id.siguienteo);
         anterior = (Button) findViewById(R.id.anteriroo);
+        odernar = (Button) findViewById(R.id.ordenar);
+        datos = (EditText) findViewById(R.id.ingresarnum);
+        mostrar = (TextView) findViewById(R.id.mostraro);
 
+        odernar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int[] num = convertirEntrada(datos.getText().toString());
+                int[] numOrdenados = ordenar(num);
+
+                String result = "";
+
+                for(int i=0;i<numOrdenados.length;i++){
+                    result = result + " " + String.valueOf(numOrdenados[i]);
+                }
+                mostrar.setText(result);
+            }
+        });
         siguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,4 +54,43 @@ public class BusquedaOrdenar extends AppCompatActivity {
             }
         });
     }
+
+    public int[] convertirEntrada(String entrada){
+
+        String[] entradas = entrada.split(" ");
+
+        int[] num = new int[entradas.length];
+
+        for(int i=0;i<entradas.length; i++){
+            try{
+                num[i] = Integer.parseInt(entradas[i]);
+            }catch(NumberFormatException e){
+                return null;
+            }
+
+        }
+        return num;
+    }
+
+    public int[] ordenar(int[] num) {
+        if(num == null){
+            Toast.makeText(this, "ERROR: Solo Numeros Pls :c", Toast.LENGTH_SHORT).show();
+            return new int[]{};
+        }
+
+        for(int i = 0; i < num.length - 1; i++)
+        {
+            for(int j = 0; j < num.length - 1; j++)
+            {
+                if (num[j] > num[j + 1])
+                {
+                    int tmp = num[j+1];
+                    num[j+1] = num[j];
+                    num[j] = tmp;
+                }
+            }
+        }
+        return num;
+    }
+
 }
--- app/src/main/java/com/rolando/tarea2_3_en_uno/Numeros.java
+++ app/src/main/java/com/rolando/tarea2_3_en_uno/Numeros.java
@@ -12,13 +12,24 @@ public class Numeros extends AppCompatActivity {
     Button anterior;
     EditText valores;
     TextView Mostrar;
+    Button calcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numeros);
         anterior = (Button) findViewById(R.id.ant);
+        valores = (EditText) findViewById(R.id.ingresarN);
+        Mostrar = (TextView) findViewById(R.id.mostrarn);
+        calcular = (Button) findViewById(R.id.buscar);
 
+        calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int dato = Integer.parseInt(valores.getText().toString());
+                Mostrar.setText(String.valueOf(serienumerica(dato)));
+            }
+        });
 
         anterior.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +38,16 @@ public class Numeros extends AppCompatActivity {
                 startActivity(anteriro);
             }
         });
+    }
+    public int serienumerica(int dato) {
+        int res = -1;
+        for(int i = 1; i <= dato; i ++){
+            if(i % 2 == 0 ){
+                res += 1;
+            }else {
+                res += 2;
+            }
+        }
+        return res;
     }
 }
